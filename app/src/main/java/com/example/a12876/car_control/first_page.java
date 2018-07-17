9a38239bd6c05b61b999f516502224ab64f55c31
亮 许 <1287674029@qq.com>
2018-07-17 19:02:06 +0800

    计算器_第一版v1.01

---
+++ app/src/main/java/com/example/a12876/car_control/first_page.java
@@ -0,0 +1,13 @@
+package com.example.a12876.car_control;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class first_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+    }
+}
